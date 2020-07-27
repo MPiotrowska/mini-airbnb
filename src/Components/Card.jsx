eafc1b828823d16353059995b27c29583b8ea8f7
MPiotrowska <monikapiotrowska27@gmail.com>
2020-07-27 16:05:04 +0200

    add responsive styles

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,15 +3,23 @@ import "./CardStyle.css";
 
 function Card(props) {
   return (
-    <section className="container-card-">
+    <section className="container-card">
       <img className="container-image" src={props.place.photo} />
       <div className="container-info">
-        <h3 className="host">{props.place.superHost}</h3>
-        <h4>{props.place.type}</h4>
-        <h4>{props.place.beds + " beds"}</h4>
+        {props.place.superHost === true && <span className="host">Super host</span>}
+        <div className="container-place">
+          <h4 className="container-place-title">{props.place.type}</h4>
+          {props.place.beds && (
+            <h4>
+              <span>.</span>
+              {props.place.beds + " beds"}
+            </h4>
+          )}
+        </div>
         <div className="container-ratings">
           <span>
-            <svg className="star"
+            <svg
+              className="star"
               xmlns="http://www.w3.org/2000/svg"
               height="20"
               viewBox=" 0 0 24 24"
